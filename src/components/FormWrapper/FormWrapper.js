2275256abc43f23aa03eae217dac6a897e9660c9
wafloski <kontakt@mateuszkonopka.pl>
2019-08-22 00:01:03 +0200

    list refactor, simple animation, handling of no image

--- src/components/FormWrapper/FormWrapper.js
+++ src/components/FormWrapper/FormWrapper.js
@@ -4,24 +4,50 @@ import styles from './FormWrapper.module.scss';
 const FormWrapper = ({submitFunc}) => (
     <div className={styles.wrapper}>
         <h2>Add new favourite person</h2>
-        <form className={styles.form} onSubmit={submitFunc}>
+        <form autoComplete="off" className={styles.form} onSubmit={submitFunc}>
             <div className={styles.item}>
-                <input type="text" className={styles.input} name="name" placeholder=" " id="name"/>
+                <input
+                    type="text"
+                    className={styles.input}
+                    name="name"
+                    placeholder=" "
+                    id="name"
+                    required
+                />
                 <label htmlFor="name" className={styles.label}>Name</label>
                 <div className={styles.itemBar}/>
             </div>
             <div className={styles.item}>
-                <input type="text" className={styles.input} name="link" placeholder=" " id="link"/>
+                <input
+                    type="text"
+                    className={styles.input}
+                    name="link"
+                    placeholder=" "
+                    id="link"
+                    required
+                />
                 <label htmlFor="link" className={styles.label}>Link</label>
                 <div className={styles.itemBar}/>
             </div>
             <div className={styles.item}>
-                <input type="text" className={styles.input} name="image" placeholder=" " id="image"/>
+                <input
+                    type="text"
+                    className={styles.input}
+                    name="image"
+                    placeholder=" "
+                    id="image"
+                />
                 <label htmlFor="image" className={styles.label}>Image</label>
                 <div className={styles.itemBar}/>
             </div>
             <div className={styles.item}>
-                <textarea name="description" className={styles.input} placeholder=" " id="description"/>
+                <textarea
+                    name="description"
+                    className={styles.textarea}
+                    placeholder=" "
+                    id="description"
+                    required
+                />
                 <label htmlFor="description" className={styles.label}>Description</label>
                 <div className={styles.itemBar}/>
             </div>
--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -7,26 +7,37 @@ const ListItem = ({
     name,
     description,
     twitterLink
-}) => (
-    <li className={styles.wrapper}>
-        <img src={image}
-             alt={name}
-             className={styles.image}
-        />
-        <div>
-            <h2 className={styles.name}>{name}</h2>
-            <p className={styles.description}>{description}</p>
-            <a href={twitterLink} className={styles.button} target="_blank" rel="noopener noreferrer">visit twitter page</a>
-        </div>
-    </li>
-);
+}) => {
+    const ImageTag = image ? "img" : "div";
+
+    return (
+        <li className={styles.wrapper}>
+            <ImageTag
+                src={image}
+                alt={name}
+                className={image ? styles.image : styles.imageNone}
+            />
+            <div>
+                <h2 className={styles.name}>{name}</h2>
+                <p className={styles.description}>{description}</p>
+                <a href={twitterLink} className={styles.button} target="_blank" rel="noopener noreferrer">visit twitter page</a>
+            </div>
+        </li>
+    )
+};
+
 
 
 ListItem.propTypes = {
-    image: PropTypes.string.isRequired,
+    image: PropTypes.string,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     twitterLink: PropTypes.string.isRequired,
 };
+
+ListItem.defaultProps = {
+    image: null,
+    description: "One of the best developers"
+}
 
 export default ListItem;
