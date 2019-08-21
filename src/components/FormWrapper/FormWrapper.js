5a72b2191e48af11ef0959c958e5cb8f54ae875f
wafloski <kontakt@mateuszkonopka.pl>
2019-08-21 22:58:54 +0200

    added styles to form

--- src/components/FormWrapper/FormWrapper.js
+++ src/components/FormWrapper/FormWrapper.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import './FormWrapper.css';
 
-const Form = ({submitFunc}) => (
-    <form onSubmit={submitFunc}>
-        <input name="name" type="text" placeholder="name"/>
-        <input name="link" type="text" placeholder="link"/>
-        <input name="image" type="text" placeholder="image"/>
-        <textarea name="description" placeholder="description"/>
-        <button type="submit">add new person</button>
-    </form>
+const FormWrapper = ({submitFunc}) => (
+    <div className="formWrapper">
+        <h2>Add new favourite person</h2>
+        <form className="formWrapper__form" onSubmit={submitFunc}>
+            <div className="formWrapper__item">
+                <input type="text" name="name" placeholder=" " id="name"/>
+                <label htmlFor="name">Name</label>
+                <div className="formWrapper__item__bar"/>
+            </div>
+            <div className="formWrapper__item">
+                <input type="text" name="link" placeholder=" " id="link"/>
+                <label htmlFor="link">Link</label>
+                <div className="formWrapper__item__bar"/>
+            </div>
+            <div className="formWrapper__item">
+                <input type="text" name="image" placeholder=" " id="image"/>
+                <label htmlFor="image">Image</label>
+                <div className="formWrapper__item__bar"/>
+            </div>
+            <div className="formWrapper__item">
+                <textarea name="description" placeholder=" " id="description"/>
+                <label htmlFor="description">Description</label>
+                <div className="formWrapper__item__bar"/>
+            </div>
+            <button type="submit" className="formWrapper__button">add new person</button>
+        </form>
+    </div>
 );
 
-export default Form;
+export default FormWrapper;
