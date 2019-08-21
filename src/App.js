ede23fcc27dba80ad7b836274c5a2f473dad18f9
wafloski <kontakt@mateuszkonopka.pl>
2019-08-21 22:26:34 +0200

    initial state of app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,47 @@ import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
 
-const App = () => (
-    <div>
-        <ListWrapper/>
-    </div>
-);
+const initialSteteItems = [
+    {
+        image: 'https://s5.tvp.pl/images2/5/1/c/uid_51c27bd35568c7475380102a44de94201547039181398_width_907_play_0_pos_0_gs_0_height_515.jpg',
+        name: 'Dan Abramov',
+        description: 'Working on @reactjs. The demo guy.',
+        twitterLink: 'https://twitter.com/dan_abramov',
+    },
+    {
+        image: 'https://s5.tvp.pl/images2/5/1/c/uid_51c27bd35568c7475380102a44de94201547039181398_width_907_play_0_pos_0_gs_0_height_515.jpg',
+        name: 'Ryan Florence',
+        description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
+        twitterLink: 'https://twitter.com/ryanflorence',
+    },
+    {
+        image: 'https://s5.tvp.pl/images2/5/1/c/uid_51c27bd35568c7475380102a44de94201547039181398_width_907_play_0_pos_0_gs_0_height_515.jpg',
+        name: 'Michael Jackson',
+        description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
+        twitterLink: 'https://twitter.com/mjackson',
+    },
+    {
+        image: 'https://s5.tvp.pl/images2/5/1/c/uid_51c27bd35568c7475380102a44de94201547039181398_width_907_play_0_pos_0_gs_0_height_515.jpg',
+        name: 'Kent C. Dodds',
+        description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
+        twitterLink: 'https://twitter.com/kentcdodds',
+    },
+];
+
+class App extends React.Component {
+    state = {
+        items: [...initialSteteItems]
+    }
+
+    render () {
+        return (
+            <div>
+                <ListWrapper
+                    items={this.state.items}
+                />
+            </div>
+        )
+    }
+}
 
 export default App;
